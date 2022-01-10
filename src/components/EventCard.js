845e8fca4d5f9fd25f6120939ec2c50e1bd91c36
Tim Quimbo <tquimbo1@gmail.com>
2022-01-10 18:58:21 -0500

    Event cardadata goes to frontend

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,4 +1,4 @@
-export default function EventCard({id, performer_name, venue_name, venue_address, datetime}){
+export default function EventCard({performer_name, venue_name, venue_address, datetime}){
   return <div className="card">
    <h2>{performer_name}</h2>
     <p>{venue_name}</p>
--- src/containers/EventIndex.js
+++ src/containers/EventIndex.js
@@ -1,12 +1,25 @@
-import {useEffect} from 'react'
+import {useEffect} from "react"
 import {getEvents} from '../redux/actionCreators'
 import { connect } from 'react-redux'
+import EventCard from "../components/EventCard"
+
 
 function EventIndex({getEvents, events}){
 
   useEffect(getEvents, [getEvents])
+// useEffect(() => events.length === 0 && getEvents(), [events])
 
-    return <form> Search by artist, event, team, or venue </form>
+    // return <form> Search by artist, event, team, or venue </form>
+
+      return <div className="cards">
+    {events.map(event => <EventCard {...event} key={event.id}/>)}
+  </div>
 }
 
-export default connect(null, { getEvents })(EventIndex)
+  const mapStateToProps = (state) => {
+  return {events: state.events}
+
+}
+
+
+export default connect(mapStateToProps, { getEvents })(EventIndex)

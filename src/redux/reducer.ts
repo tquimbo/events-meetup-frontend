1f9f38d2c8afd63f15c155a47bdb42785d13a1dd
Tim Quimbo <tquimbo1@gmail.com>
2023-04-19 11:59:06 -0400

    Added Search Reducers.

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -207,6 +207,27 @@ const initialState = {
     }
 }
 
+import { SEARCH_EVENTS_REQUEST, SEARCH_EVENTS_SUCCESS, SEARCH_EVENTS_FAILURE } from '../actions/searchActions';
+
+const initialState = {
+  loading: false,
+  results: [],
+  error: '',
+};
+
+export const searchReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    case SEARCH_EVENTS_REQUEST:
+      return { ...state, loading: true };
+    case SEARCH_EVENTS_SUCCESS:
+      return { ...state, loading: false, results: action.payload, error: '' };
+    case SEARCH_EVENTS_FAILURE:
+      return { ...state, loading: false, results: [], error: action.payload };
+    default:
+      return state;
+  }
+};
+
 
   export default exp
 

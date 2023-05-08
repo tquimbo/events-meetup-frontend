2973200b5224e58b985a85134c1b046cc625464b
Tim Quimbo <tquimbo1@gmail.com>
2023-05-08 08:53:18 -0400

    Event shows amount of users attending.

--- src/components/Attendees.js
+++ src/components/Attendees.js
@@ -4,9 +4,14 @@ import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { getEvent } from '../redux/actionCreators.ts';
 import { ActionCableConsumer } from 'react-actioncable-provider';
+import { connect } from 'react-redux';
 
 
-const Attendees = ({ events, users }) => {
+const Attendees = (props) => {
+
+
+  const event = props.event
+ 
   const [showModal, setShowModal] = useState(false);
 
   const handleClose = () => setShowModal(false);
@@ -20,40 +25,57 @@ const Attendees = ({ events, users }) => {
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
-        Attendees: {events.users.length}
+        {/* Attendees: {event.users.length} */}
+        Attendees: {event.users ? event.users.length : 0}
       </Button>
 
-      <Modal show={showModal} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Attendees</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <ListGroup>
-            {users.map((user) => (
-              <ListGroup.Item key={user.id}>
-                {/* <Image
-                  src={user.profile_picture}
-                  roundedCircle
-                  width="30"
-                  height="30"
-                  className="mr-2"
-                /> */}
-                <Link to={`/users/${user.id}`} onClick={handleClose}>
-                  {user.first_name} {user.last_name}
-                </Link>
-              </ListGroup.Item>
-            ))}
-          </ListGroup>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-       
-        </Modal.Footer>
-      </Modal>
     </>
   );
 };
 
-export default Attendees;
+
+
+
+const mapStateToProps = (state) => {
+  return {
+     user: state.user, 
+     users: state.users, 
+     event: state.event,
+     events: state.events,
+     userEvents: state.user.userEvents
+  }
+      
+}
+
+export default connect(mapStateToProps, { getEvent })(Attendees)
+
+
+{/* <Modal show={showModal} onHide={handleClose}>
+<Modal.Header closeButton>
+  <Modal.Title>Attendees</Modal.Title>
+</Modal.Header>
+<Modal.Body>
+  <ListGroup>
+    {events.users.map((user) => (
+      <ListGroup.Item key={user.id}>
+        {/* <Image
+          src={user.profile_picture}
+          roundedCircle
+          width="30"
+          height="30"
+          className="mr-2"
+        /> */}
+//         <Link to={`/users/${user.id}`} onClick={handleClose}>
+//           {user.first_name} {user.last_name}
+//         </Link>
+//       </ListGroup.Item>
+//     ))}
+//   </ListGroup>
+// </Modal.Body>
+{/* <Modal.Footer>
+  <Button variant="secondary" onClick={handleClose}>
+    Close
+  </Button> */}
+
+// </Modal.Footer>
+// </Modal> */}

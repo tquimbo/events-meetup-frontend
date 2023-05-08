ac532acf99634d24d5a4223959357488129aaa51
Tim Quimbo <tquimbo1@gmail.com>
2023-05-08 15:37:45 -0400

    Attending correctly shows when user already is attending.

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -17,53 +17,130 @@ import MyProfile from "../components/MyProfile";
 
 
 
-
-function AddEvent(props){
-
+function AddEvent(props) {
 
   let navigate = useNavigate();
 
-  
-    const user = props.user
-    const event = props.event
+  const user = props.user;
+  const event = props.event;
    
-    const userID = props.user.id
-    const username = props.user.username
+  const userID = props.user.id;
+  const username = props.user.username;
     
-    
+  const isEventAlreadyAdded = () => {
+    return event.users.some((u) => u.id === user.id);
+  };
 
-    const handleClick = (e) => {
-        e.preventDefault()
-        props.addEvent({ user, event } )
-      }
+  const [isAttending, setIsAttending] = useState(isEventAlreadyAdded());
 
-    
-
-  
-    return (<div className="addEvent">
-
-
-            <button onClick={handleClick}>
-            Attending
-            </button>            
-    
-   </div>
-    )
-};
-
-
-
-const mapStateToProps = (state) => {
-    return {
-       user: state.user, 
-       event: state.event,
-       userEvents: state.user.userEvents
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (!isEventAlreadyAdded()) {
+      props.addEvent({ user, event });
+      setIsAttending(true);
+    } else {
+      alert("You have already added this event.");
     }
-        
+  };
+
+  return (
+    <div className="addEvent">
+      <button onClick={handleClick}>{isAttending ? "Attending" : "Add Event"}</button>
+    </div>
+  );
 }
 
+const mapStateToProps = (state) => {
+  return {
+    user: state.user, 
+    event: state.event,
+  }
+};
 
-// export default connect(null, { submitSignup, submitLogin, getEvent, addEvent })(AddEvent)
-export default connect(mapStateToProps, { submitSignup, submitLogin, getEvent, addEvent })(AddEvent)
+export default connect(mapStateToProps, { getEvent, addEvent })(AddEvent);
+
+// function AddEvent(props){
 
 
+//   let navigate = useNavigate();
+
+  
+//     const user = props.user
+//     const event = props.event
+   
+//     const userID = props.user.id
+//     const username = props.user.username
+    
+//     const isEventAlreadyAdded = () => {
+//       return user.userEvents.some((userEvent) => userEvent.id === event.id);
+//     };
+
+//     const [isAttending, setIsAttending] = useState(isEventAlreadyAdded());
+
+  
+//     // const handleClick = (e) => {
+//     //   e.preventDefault();
+//     //   if (!isEventAlreadyAdded()) {
+//     //     props.addEvent({ user, event });
+//     //   } else {
+//     //     alert("You have already added this event.");
+//     //   }
+//     // };
+    
+//   // const handleClick = (e) => {
+//   //   e.preventDefault();
+//   //   if (!isEventAlreadyAdded()) {
+//   //     props.addEvent({ user, event });
+//   //     setIsAttending(true);
+//   //   } else {
+//   //     alert("You have already added this event.");
+//   //   }
+//   // };
+//   const handleClick = (e) => {
+//     e.preventDefault();
+//     if (isEventAlreadyAdded()){
+//       setIsAttending(true);}
+//     else if (!isEventAlreadyAdded()) {
+//       props.addEvent({ user, event });
+//       setIsAttending(true);
+//     }
+//      else {
+//       alert("You have already added this event.");
+//     }
+//   };
+
+
+//     // const handleClick = (e) => {
+//     //     e.preventDefault()
+//     //     props.addEvent({ user, event } )
+//     //   }
+
+    
+
+  
+//     return (<div className="addEvent">
+
+
+//             {/* <button onClick={handleClick}>
+//             Attending
+//             </button>             */}
+//                   {/* <button onClick={handleClick}>{isAttending ? "Attending" : "Add Event"}</button> */}
+//                   <button {isAttending ? "Attending" : "Add Event"}</button>
+//    </div>
+//     )
+// };
+
+
+
+// const mapStateToProps = (state) => {
+//     return {
+//        user: state.user, 
+//        event: state.event,
+//       //  userEvents: state.user.userEvents
+//     }
+        
+// }
+
+
+// // export default connect(null, { submitSignup, submitLogin, getEvent, addEvent })(AddEvent)
+// export default connect(mapStateToProps, { getEvent, addEvent })(AddEvent);

82090626f224e4e8b455ccdceeb709d63e95900e
Tim Quimbo <tquimbo1@gmail.com>
2023-05-11 14:32:30 -0400

    Search gets results from SeatGeek API.

--- src/redux/searchActions.ts
+++ src/redux/searchActions.ts
@@ -1,35 +1,4 @@
-// export const SEARCH_REQUEST = 'SEARCH_REQUEST';
-// export const SEARCH_SUCCESS = 'SEARCH_SUCCESS';
-// export const SEARCH_FAILURE = 'SEARCH_FAILURE';
 
-// export const searchRequest = () => ({
-//   type: SEARCH_REQUEST,
-// });
-
-// export const searchSuccess = (results) => ({
-//   type: SEARCH_SUCCESS,
-//   payload: results,
-// });
-
-// export const searchFailure = (error) => ({
-//   type: SEARCH_FAILURE,
-//   payload: error,
-// });
-
-
-// export const search = (query) => async (dispatch) => {
-//   dispatch(searchRequest());
-//   try {
-//     // const response = await fetch(`http://localhost:3000/api/v1/search?query=${query}`);
-//     const response = await fetch(`http://localhost:3000/api/v1/query=${query}`);
-//     const results = await response.json();
-//     dispatch(searchSuccess(results));
-//   } catch (error) {
-//     dispatch(searchFailure(error));
-//   }
-// };
-
-// searchActions.js
 export const SEARCH_REQUEST = 'SEARCH_REQUEST';
 export const SEARCH_SUCCESS = 'SEARCH_SUCCESS';
 export const SEARCH_FAILURE = 'SEARCH_FAILURE';
